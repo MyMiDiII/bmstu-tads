e066ff2689c9cf375d8a0cd52ff3f4b1a82c4003
mmd19u540 <mymidi.student@yandex.ru>
2020-09-20 21:53:43 +0300

    FIX: read max order and read mantissa with point

--- lab_01/big_nums.c
+++ lab_01/big_nums.c
@@ -23,12 +23,14 @@ int read_mantissa(big_double *const number)
 
     while ((ch = getchar()) != 'e' && ch != 'E' && ch != '\n' && ch != EOF)
     {
+        // ? как хранить ноль
+        // ! убирать нули в начале и конце
+
         if (ch == ' ')
             continue;
 
         if (begin)
         {
-            // добавить обработку случая . e k
             if ('-' == ch || '+' == ch)
             {
                 number->sign = ch;
@@ -47,15 +49,16 @@ int read_mantissa(big_double *const number)
         if (point_flag)
             current_point_place++;
 
-        if (ch == '.' && begin)
+        if (ch == '.')
             point_flag = true;
 
         else if (ch < '0' || ch > '9')
             return ERR_WRONG_CHAR;
 
-        // добавить проверку на огромное количество нулей
         else
         {
+            if (number->len_num >= MAX_DOUBLE_LEN)
+                return ERR_WRONG_LEN;
             number->num[number->len_num] = char_into_int(ch);
             number->len_num++;
         }
@@ -67,7 +70,7 @@ int read_mantissa(big_double *const number)
     if (!number->len_num && !point_flag)
         return ERR_EMPTY_MANTISSA;
 
-    number->point_place = current_point_place;
+    number->point_place = number->len_num - current_point_place;
 
     return READ_OK;
 }
@@ -80,11 +83,16 @@ int read_order(int *const order)
 
     while ((ch = getchar()) != '\n' && ch != EOF && count < MAX_ORDER_LEN + 1)
     {
+        if (ch == ' ')
+            continue;
+
+        if (0 == count && ch != '-' && ch != '+')
+            read_order[count++] = '+';
         read_order[count] = ch;
         count++;
     }
 
-    if (count > MAX_ORDER_LEN + 1)
+    if (count > MAX_ORDER_LEN)
         return ERR_READ_ORDER;
 
     read_order[count] = '\0';
@@ -93,7 +101,7 @@ int read_order(int *const order)
 
     *order = strtol(read_order, &end_ptr, 10);
 
-    if (end_ptr == read_order)
+    if (end_ptr == read_order || *end_ptr != '\0')
         return ERR_READ_ORDER;
 
     return READ_OK;
@@ -105,8 +113,29 @@ int read_big_double(big_double *const number)
 
     exit_code = read_mantissa(number);
 
+    // printf("%d\n", exit_code);
+
     if (!exit_code)
         exit_code = read_order(&number->order);
+
+    // printf("%d\n", exit_code);
+/*
+    if (!exit_code)
+    {
+        for (int i = 0; i < number->len_num; i++)
+        {
+            if (i == number->point_place)
+            {
+                printf(".%d", number->num[i]);
+            }
+            else
+            {
+                printf("%d", number->num[i]);
+            }
+        }
+
+        printf("e%d\n", number->order);
+    }*/
 
     return exit_code; 
 }
