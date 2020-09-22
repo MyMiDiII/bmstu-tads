f6c3eccf299c3b69956c6fcfe4f5ab7c60d70e11
mmd19u540 <mymidi.student@yandex.ru>
2020-09-22 23:12:59 +0300

    ADD: errors documentation

--- lab_01/errors.h
+++ lab_01/errors.h
@@ -6,6 +6,13 @@
 
 #define __ERRORS_H__
 
+/**
+ * @brief print_error()
+ * 
+ * Печатает сообщение об ошибке согласно её коду
+ * 
+ * @param [in] code Код ошибки
+ */
 void print_error(const int code);
 
 #endif
