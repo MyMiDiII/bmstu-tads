bcfc061110a8270e72b496ab7e568a54545db902
mmd19u540 <mymidi.student@yandex.ru>
2020-10-04 11:51:36 +0300

    ADD: reading file name and opening this file

--- lab_02/cars.c
+++ lab_02/cars.c
@@ -2,35 +2,40 @@
 #include <stdlib.h>
 
 #include "cars.h"
+#include "errors.h"
 
 int upload_from_file(car_table_t *table)
 {
     char *file_name = NULL;
     size_t len = 0;
     char ch;
+    int read;
 
     // ! после нормального считывания пункта меню убрать
     scanf("%c", &ch);
     puts("Введите имя файла (с путем при нахождении файла в другой директории):");
 
-    if ((getline(&file_name, &len, stdin)) != -1)
+    if ((read = getline(&file_name, &len, stdin)) != -1)
     {
+        if ('\n' == file_name[read - 1])
+            file_name[read - 1] = '\0';
+
+        // for (int i = 0; i < read - 1; i++)
+        //    printf("%c", file_name[i]);
+
         FILE *file = fopen(file_name, "r");
+        free(file_name);
         
-        if (!file)
+        if (file)
         {
-            puts("Файл открыт!");
+            // считать данные из файла
 
             if (fclose(file) == EOF)
                 puts("Произошла неведомая хрень!");
         }
         else
-            puts("Сударь, Вы ошиблись!");
-
-
-        free(file_name);
+            return ERR_OPEN_FILE;
     }
     else
-        // ! убрать Сударя
-        puts("Ну Вы молодец, Сударь, ведь ничего не работает");
+        return ERR_READ_FILE_NAME;
 }
